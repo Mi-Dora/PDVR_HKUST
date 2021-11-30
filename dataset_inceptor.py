263acd1d201a416373fa1e7172448243c3d678b8
midora <mi-dora@qq.com>
2021-11-30 22:47:24 +0800

    -loss curve

--- dataset_inceptor.py
+++ dataset_inceptor.py
@@ -1,4 +1,7 @@
 import numpy as np
+import pickle
+
+b = pickle.load('datasets/cc_web_video.pickle', 'r')
 
 feature = np.load('output_data/1_vgg.npy')
 triplet = np.load('output_data/cc_web_video_triplets.npy')
