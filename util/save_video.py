83a0a4a14fe0779c638fdbff1109d5dbf63a8291
midora <mi-dora@qq.com>
2021-12-01 18:43:03 +0800

    -video writer

--- util/save_video.py
+++ util/save_video.py
@@ -1,7 +1,7 @@
 import cv2
 
 
-def save_video(frame_list, save_path, fps=30,  greyscale=False):
+def save_video(list_file, save_path, fps=30,  greyscale=False):
     """
     Args:
         frame_list: (list of ndarray or ndarray) frame list
@@ -9,10 +9,13 @@ def save_video(frame_list, save_path, fps=30,  greyscale=False):
         fps: frame per second
         greyscale: whether save grey scale video
     """
+    with open(list_file, 'r') as f:
+        lines = f.readlines()
+    img = cv2.imread(lines[0])
     if greyscale:
-        H, W = frame_list[0].shape
+        H, W = img.shape
     else:
-        H, W, _ = frame_list[0].shape
+        H, W, _ = img.shape
     size = (int(W), int(H))
     encoder = cv2.VideoWriter_fourcc(*'mp4v')
     writer = cv2.VideoWriter()
@@ -21,7 +24,8 @@ def save_video(frame_list, save_path, fps=30,  greyscale=False):
     else:
         writer.open(save_path, encoder, fps=fps, frameSize=size, isColor=True)
 
-    for frame in frame_list:
+    for line in lines:
+        frame = cv2.imread(line)
         writer.write(frame)
 
     print(save_path + ' saved.')
